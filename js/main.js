f70b0b0fbcc65a7a7e9ac6dbf4a7cf2964d1802c
Ella0707 <pakelvi77@gmail.com>
2022-06-05 23:54:35 +0300

    work

--- js/main.js
+++ js/main.js
@@ -11,70 +11,110 @@ function show_hide_password(target) {
 }
 
 
-window.addEventListener("DOMContentLoaded", function() {
-    [].forEach.call( document.querySelectorAll('.tel'), function(input) {
-    var keyCode;
-    function mask(event) {
-        event.keyCode && (keyCode = event.keyCode);
-        var pos = this.selectionStart;
-        if (pos < 3) event.preventDefault();
-        var matrix = "+7 (___) ___ ____",
-            i = 0,
-            def = matrix.replace(/\D/g, ""),
-            val = this.value.replace(/\D/g, ""),
-            new_value = matrix.replace(/[_\d]/g, function(a) {
-                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-            });
-        i = new_value.indexOf("_");
-        if (i != -1) {
-            i < 5 && (i = 3);
-            new_value = new_value.slice(0, i)
+window.addEventListener("DOMContentLoaded", function () {
+    [].forEach.call(document.querySelectorAll('.tel'), function (input) {
+        var keyCode;
+        function mask(event) {
+            event.keyCode && (keyCode = event.keyCode);
+            var pos = this.selectionStart;
+            if (pos < 3) event.preventDefault();
+            var matrix = "+7 (___) ___ ____",
+                i = 0,
+                def = matrix.replace(/\D/g, ""),
+                val = this.value.replace(/\D/g, ""),
+                new_value = matrix.replace(/[_\d]/g, function (a) {
+                    return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+                });
+            i = new_value.indexOf("_");
+            if (i != -1) {
+                i < 5 && (i = 3);
+                new_value = new_value.slice(0, i)
+            }
+            var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+                function (a) {
+                    return "\\d{1," + a.length + "}"
+                }).replace(/[+()]/g, "\\$&");
+            reg = new RegExp("^" + reg + "$");
+            if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+            if (event.type == "blur" && this.value.length < 5) this.value = ""
         }
-        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-            function(a) {
-                return "\\d{1," + a.length + "}"
-            }).replace(/[+()]/g, "\\$&");
-        reg = new RegExp("^" + reg + "$");
-        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-        if (event.type == "blur" && this.value.length < 5)  this.value = ""
-    }
 
-    input.addEventListener("input", mask, false);
-    input.addEventListener("focus", mask, false);
-    input.addEventListener("blur", mask, false);
-    input.addEventListener("keydown", mask, false)
-
-  });
-
+        input.addEventListener("input", mask, false);
+        input.addEventListener("focus", mask, false);
+        input.addEventListener("blur", mask, false);
+        input.addEventListener("keydown", mask, false)
+    });
 });
 
-// div.className = "alert";
-//   div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
-
-//   document.body.append(div);
 
 
-// let inputINN = document.querySelector(".input-inn");
-// let buttonForm = document.querySelector(".registration__btn");
-// let out = document.querySelector(".registration__warning");
+window.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+    let inputINN = document.querySelector(".input-inn");
+    let out = document.querySelector(".out");
+    let outLink = document.querySelector(".link");
 
+    async function formSend(e) {
+        e.preventDefault();
 
-// buttonForm.onclick = function() {
-//         valueInput = (inputINN.value);
-//         // console.log (valueInput)
-//         if (inputINN = "111") {
-//             out.innerHTML = ('Ваша компания уже зарегистрирована, пожалуйста обратитесь&nbsp;к менеджеру вашего аккаунта или' );
-//             // document.body.append(el);
-//             // inputINN.setAttribute("novalidate");
-//             buttonForm.setAttribute("disabled", "disabled");
+        let error = formValidate(form);
 
-//         }
-//     //    else {
-//     //     inputINN = "";
-//     //     buttonForm.setAttribute("defoult", "defoult");
+        if (error === 0) {
+            // код для отправки формы
 
-//     //    }
-    
-// }
+        } else {
+            alert("Заполните, пожалуйста, обязательные поля");
+        }
+    }
 
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll(".req");
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains(".email"));
+            if (emailTest(input)) {
+                formAddError(input);
+                error++;
+            } 
+
+            if (input.classList.contains(".input-inn"));
+            if (innTest(input)) {
+                out.innerHTML = "Ваша компания уже зарегистрирована,пожалуйста обратитесь&nbsp;к менеджеру вашего аккаунта или ";
+                outLink.innerHTML = "напишите нам";
+                
+                error++;
+            } 
+            
+            else {
+                if (input.value === "") {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+        return error;
+    }
+
+    function formAddError(input) {
+        input.parentElement.classList.add('error');
+        input.classList.add('error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('error');
+        input.classList.remove('error');
+    }
+
+    function emailTest(input) {
+        return /.+@globex\.com/.test(input.value);
+    }
+
+    function innTest(input) {
+        return (input.value);
+    }
+});
 
